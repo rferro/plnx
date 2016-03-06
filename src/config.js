db846419c7993bf6ecec38db037010b56fb6deec
rferro <ricardo.ferro@gmail.com>
2016-03-06 05:50:26 -0300

    First version

---
+++ src/config.js
@@ -0,0 +1,144 @@
+
+export default {
+  url: {
+    public:   "https://poloniex.com/public",
+    private:  "https://poloniex.com/tradingApi",
+    push:     "wss://api.poloniex.com"
+  },
+  commands: {
+    // public
+    returnTicker: {
+      type:   "public",
+      params: [],
+    },
+    return24Volume: {
+      type:   "public",
+      params: [],
+    },
+    return24hVolume: {
+      type:   "public",
+      params: [],
+    },
+    returnOrderBook: {
+      type:   "public",
+      params: ["currencyPair", "depth?"],
+    },
+    returnChartData: {
+      type:   "public",
+      params: ["currencyPair", "start", "end", "period"],
+    },
+    returnCurrencies: {
+      type:   "public",
+      params: [],
+    },
+    returnLoanOrders: {
+      type:   "public",
+      params: ["currency"],
+    },
+    // private
+    returnBalances: {
+      type:   "private",
+      params: [],
+    },
+    returnCompleteBalances: {
+      type:   "private",
+      params: [],
+    },
+    returnDepositAddresses: {
+      type:   "private",
+      params: [],
+    },
+    generateNewAddress: {
+      type:   "private",
+      params: ["currency"],
+    },
+    returnDepositsWithdrawals: {
+      type:   "private",
+      params: ["start", "end"],
+    },
+    returnOpenOrders: {
+      type:   "private",
+      params: ["currencyPair"],
+    },
+    buy: {
+      type:   "private",
+      params: ["currencyPair", "rate", "amount", "fillOrKill?", "immediateOrCancel?"],
+    },
+    sell: {
+      type:   "private",
+      params: ["currencyPair", "rate", "amount", "fillOrKill?", "immediateOrCancel?"],
+    },
+    cancelOrder: {
+      type:   "private",
+      params: ["orderNumber"],
+    },
+    moveOrder: {
+      type:   "private",
+      params: ["orderNumber", "rate"],
+    },
+    withdraw: {
+      type:   "private",
+      params: ["currency", "amount", "address", "paymentId?"],
+    },
+    returnAvailableAccountBalances: {
+      type:   "private",
+      params: ["account?"],
+    },
+    returnTradableBalances: {
+      type:   "private",
+      params: [],
+    },
+    transferBalance: {
+      type:   "private",
+      params: ["currency", "amount", "fromAccount", "toAccount"],
+    },
+    returnMarginAccountSummary: {
+      type:   "private",
+      params: [],
+    },
+    marginBuy: {
+      type:   "private",
+      params: ["currencyPair", "rate", "amount", "lendingRate?"],
+    },
+    marginSell: {
+      type:   "private",
+      params: ["currencyPair", "rate", "amount", "lendingRate?"],
+    },
+    getMarginPosition: {
+      type:   "private",
+      params: ["currencyPair", "type?", "liquidationPrice?"],
+    },
+    closeMarginPosition: {
+      type:   "private",
+      params: ["currencyPair"],
+    },
+    createLoanOffer: {
+      type:   "private",
+      params: ["currency", "amount", "duration", "autoRenew", "lendingRate"],
+    },
+    cancelLoanOffer: {
+      type:   "private",
+      params: ["orderNumber"],
+    },
+    returnOpenLoanOffers: {
+      type:   "private",
+      params: [],
+    },
+    returnActiveLoans: {
+      type:   "private",
+      params: [],
+    },
+    toggleAutoRenew: {
+      type:   "private",
+      params: ["orderNumber"]
+    },
+    // public and private
+    returnTradeHistory: {
+      type:   "both",
+      params: {
+        private: ["currencyPair", "start?", "end?"],
+        public:  ["currencyPair", "start?", "end?"]
+      }
+    },
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,99 @@
+
+import crypto       from "crypto";
+import querystring  from "querystring";
+
+import debug        from "debug";
+import request      from "request";
+import nonce        from "nonce";
+
+import config       from "./config";
+
+let dbg = debug("plnx");
+let exp = {};
+
+for (let command in config.commands) {
+  let cfg = config.commands[command];
+
+  exp[command] = function(opt, cb) {
+    let ks         = typeof opt.key === "string" && typeof opt.secret === "string";
+    let is_private = cfg.type === "private" || (cfg.type === "both" && ks);
+
+    if (arguments.length === 1) {
+      cb  = opt;
+      opt = {};
+    }
+
+    if (typeof cb !== "function")
+      throw new Error(`${command}: callback is not a function`);
+
+    if (is_private && !ks)
+      return cb(`${command}: opt.key and opt.secret are necessary`);
+
+    let key    = opt.key;
+    let secret = opt.secret;
+
+    delete opt.key;
+    delete opt.secret;
+
+    let missing = [];
+
+    if (cfg.type === "both") {
+      cfg.params = cfg.params[is_private ? "private" : "public"];
+    }
+
+    for (let param of cfg.params) {
+      let required = param.slice(-1) !== "?";
+
+      if (required && typeof opt[param] === "undefined")
+        missing.push(param);
+    }
+
+    if (missing.length)
+      return cb(`${command}: ${missing} required`);
+
+    let ropt = { json: true };
+
+    opt["command"] = command;
+    opt["nonce"]   = nonce()();
+
+    if (is_private) {
+      ropt["method"]   = "POST";
+      ropt["url"]      = config.url.private;
+      ropt["form"]     = opt;
+      ropt["headers"]  = {
+        Key:  key,
+        Sign: crypto
+          .createHmac('sha512', new Buffer(secret))
+          .update(querystring.stringify(opt))
+          .digest('hex')
+      };
+    }
+    else {
+      ropt["method"] = "GET";
+      ropt["url"]    = config.url.public;
+      ropt["qs"]     = opt;
+    }
+
+    request(ropt, (err, res, data) => {
+      if (!err && data && data.error)
+        err = data.error;
+
+      if (err)
+        return cb(err);
+
+      cb(null, data);
+    });
+
+    dbg({
+      key,
+      secret,
+      opt,
+      is_private
+    });
+  };
+}
+
+// fix poloniex api docs
+exp.return24Volume = exp.return24hVolume;
+
+export default exp;
