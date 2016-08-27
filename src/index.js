6b7560289552f202960c853d62f1d4d7b077839c
rferro <ricardo.ferro@gmail.com>
2016-08-27 03:13:47 -0300

    Fix nonce problem using async.queue

--- src/index.js
+++ src/index.js
@@ -7,11 +7,86 @@ import debug from 'debug'
 import request from 'request'
 import autobahn from 'autobahn'
 import _ from 'lodash'
+import async from 'async'
 
 import config from './config'
 import nonce from './nonce'
 
 let dbg = debug(pkg.name)
+
+let queue = async.queue(({ cfg, command, options }, cb) => {
+  let opt = _.cloneDeep(options)
+  let ks = typeof opt.key === 'string' && typeof opt.secret === 'string'
+  let is_private = cfg.type === 'private' || (cfg.type === 'both' && ks)
+
+  if (typeof cb !== 'function') {
+    throw new Error(`${command}: callback is not a function`)
+  }
+
+  if (is_private && !ks) {
+    return cb(`${command}: options.key and options.secret are required`)
+  }
+
+  let key = opt.key
+  let secret = opt.secret
+
+  delete opt.key
+  delete opt.secret
+
+  let missing = []
+  let params = cfg.type === 'both'
+    ? cfg.params[is_private ? 'private' : 'public']
+    : cfg.params
+
+  for (let param of params) {
+    if (param.slice(-1) !== '?' && typeof opt[param] === 'undefined') {
+      missing.push(param)
+    }
+  }
+
+  if (missing.length) {
+    return cb(`${command}: options ${missing} are required`)
+  }
+
+  let ropt = {
+    json: true,
+    headers: {
+      'User-Agent': `${pkg.name} ${pkg.version}`
+    }
+  }
+
+  opt.command = command
+
+  if (is_private) {
+    opt.nonce = nonce()
+    ropt.method = 'POST'
+    ropt.url = config.urls.private
+    ropt.form = opt
+    ropt.headers.Key = key
+    ropt.headers.Sign = crypto
+      .createHmac('sha512', new Buffer(secret))
+      .update(querystring.stringify(opt))
+      .digest('hex')
+  } else {
+    ropt.method = 'GET'
+    ropt.url = config.urls.public
+    ropt.qs = opt
+  }
+
+  dbg({ key, secret, opt, is_private, ropt })
+
+  request(ropt, (err, res, data) => {
+    if (err) {
+      cb(err)
+    } else if (res.statusCode !== 200) {
+      cb(`statusCode ${res.statusCode}`)
+    } else if (data && data.error) {
+      cb(data.error)
+    } else {
+      cb(null, data)
+    }
+  })
+})
 
 for (let command in config.commands) {
   let cfg = config.commands[command]
@@ -22,77 +97,7 @@ for (let command in config.commands) {
       options = {}
     }
 
-    let opt = _.cloneDeep(options)
-    let ks = typeof opt.key === 'string' && typeof opt.secret === 'string'
-    let is_private = cfg.type === 'private' || (cfg.type === 'both' && ks)
-
-    if (typeof cb !== 'function') {
-      throw new Error(`${command}: callback is not a function`)
-    }
-
-    if (is_private && !ks) {
-      return cb(`${command}: options.key and options.secret are required`)
-    }
-
-    let key = opt.key
-    let secret = opt.secret
-
-    delete opt.key
-    delete opt.secret
-
-    let missing = []
-    let params = cfg.type === 'both'
-      ? cfg.params[is_private ? 'private' : 'public']
-      : cfg.params
-
-    for (let param of params) {
-      if (param.slice(-1) !== '?' && typeof opt[param] === 'undefined') {
-        missing.push(param)
-      }
-    }
-
-    if (missing.length) {
-      return cb(`${command}: options ${missing} are required`)
-    }
-
-    let ropt = {
-      json: true,
-      headers: {
-        'User-Agent': `${pkg.name} ${pkg.version}`
-      }
-    }
-
-    opt.command = command
-
-    if (is_private) {
-      opt.nonce = nonce()
-      ropt.method = 'POST'
-      ropt.url = config.urls.private
-      ropt.form = opt
-      ropt.headers.Key = key
-      ropt.headers.Sign = crypto
-        .createHmac('sha512', new Buffer(secret))
-        .update(querystring.stringify(opt))
-        .digest('hex')
-    } else {
-      ropt.method = 'GET'
-      ropt.url = config.urls.public
-      ropt.qs = opt
-    }
-
-    dbg({ key, secret, opt, is_private, ropt })
-
-    request(ropt, (err, res, data) => {
-      if (err) {
-        cb(err)
-      } else if (res.statusCode !== 200) {
-        cb(`statusCode ${res.statusCode}`)
-      } else if (data && data.error) {
-        cb(data.error)
-      } else {
-        cb(null, data)
-      }
-    })
+    queue.push({ cfg, command, options }, cb)
   }
 }
 
